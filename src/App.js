9d37b5e2d31d8fd88465e8860633a274fad564b6
T-Beard <thomasspalsson@gmail.com>
2020-01-10 03:18:01 +0000

    Initial setup
    Laying the foundations for future work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Helmet from 'react-helmet';
+
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <main>
+      <Helmet title="T-Bone Zone" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello</p>
       </header>
-    </div>
+    </main>
   );
 }
 
