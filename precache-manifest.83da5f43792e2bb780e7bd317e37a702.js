932c93a74446ffefb7ca4e9c510ff2645262791f
T-Beard <thomasspalsson@gmail.com>
2020-01-20 14:12:19 +0000

    Updates

---
+++ precache-manifest.83da5f43792e2bb780e7bd317e37a702.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0d373ecf8d8f9578071f5fad26974c",
+    "url": "/whodis/index.html"
+  },
+  {
+    "revision": "0d526f98e3accd2ad86f",
+    "url": "/whodis/static/css/main.27a1056b.chunk.css"
+  },
+  {
+    "revision": "318f69888e56b955d723",
+    "url": "/whodis/static/js/2.9aedc307.chunk.js"
+  },
+  {
+    "revision": "c2ca14468f78f908c37847bb4ba35cc2",
+    "url": "/whodis/static/js/2.9aedc307.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d526f98e3accd2ad86f",
+    "url": "/whodis/static/js/main.67f6226b.chunk.js"
+  },
+  {
+    "revision": "2bafddffdc910383cc6d",
+    "url": "/whodis/static/js/runtime-main.9bbc8c1b.js"
+  },
+  {
+    "revision": "4d0c761fe97885e97e379b41c506c689",
+    "url": "/whodis/static/media/laptop.4d0c761f.jpg"
+  }
+]);
